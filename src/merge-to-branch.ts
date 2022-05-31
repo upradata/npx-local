7498c6e6db58647ef6684c57f9045d97cf1cf617
Thomas Milotti <thomas.milotti@gmail.com>
2022-05-31 10:48:00 +0200

    enhance merge-to-branch

--- src/merge-to-branch.ts
+++ src/merge-to-branch.ts
@@ -1,37 +1,61 @@
+import { styles as s } from '@upradata/node-util';
 import shell from 'shelljs';
+import live from 'shelljs-live';
 import { LocalInstall } from './local-install';
+
+type LogOptions = { newLine?: boolean; };
+
+const log = (str: string, what: 'log' | 'warn' | 'error', options: LogOptions = {}) => {
+    const { newLine = true } = options;
+    console[ what ](`${newLine ? '\n' : ''}${str}`);
+};
+
+const inform = (str: string, options?: LogOptions) => log(s.magenta.bold.$`⦿ ${str} ⦿`, 'log', options);
+
+const error = (str: string, options?: LogOptions) => log(s.red.bold.$`❌ ${str} ❌`, 'error', options);
+
 
 export const isGitInstalled = () => shell.which('git');
 
 export const ensureGitInstalled = () => {
     if (!isGitInstalled()) {
-        shell.echo('Sorry, this script requires git');
+        error('Sorry, this script requires git');
         shell.exit(1);
     }
 };
 
-export const mergeInto = (branchName: string = 'master', options: { bumpVersion?: boolean; npmPublish?: boolean; } = {}) => {
+
+export const mergeInto = async (branchName: string = 'master', options: { bumpVersion?: boolean; npmPublish?: boolean; } = {}) => {
     ensureGitInstalled();
 
     const currentBranch = shell.exec('git branch --show-current', { silent: true }).stdout.trim();
 
     if (options.bumpVersion) {
-        shell.echo('Bump version: patch');
-        shell.exec('npm version patch');
+        inform('Bump version: patch');
+        live('npm version patch');
     }
 
-    shell.exec(`git checkout ${branchName}`);
-    shell.exec(`git merge ${currentBranch}`);
-    shell.exec('git checkout --theirs .');
+    inform(`Moving to ${branchName}`);
+    live(`git checkout ${branchName}`);
 
-    new LocalInstall().copyLocalDepsToNpmProperty();
+    inform(`Git merge: ${currentBranch} ⟶ ${branchName}`);
+    live(`git merge ${currentBranch}`);
+    live('git checkout --theirs .');
 
-    shell.exec('git add .');
-    shell.exec(`git commit -m "merge ${currentBranch}"`);
-    shell.exec('git pushall && git pushall-tags');
+    inform(`Execute npx-local local-to-npm`);
+    await new LocalInstall({ quiet: true }).copyLocalDepsToNpmProperty();
 
-    if (options.npmPublish)
-        shell.exec('npm publish --access public');
+    inform('Commit merge + Git push (commits and tags)');
 
-    shell.exec(`git checkout ${currentBranch}`);
+    live('git add .');
+    live(`git commit -m "merge ${currentBranch}"`);
+    live('git pushall && git pushall-tags');
+
+    if (options.npmPublish) {
+        inform('Publish to NPM registry');
+        live('npm publish --access public');
+    }
+
+    inform(`Moving to ${currentBranch}`);
+    live(`git checkout ${currentBranch}`);
 };
